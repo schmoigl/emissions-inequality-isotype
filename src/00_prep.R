7aac660d54a7a490b292b1754f8a3f349bd6e7e7
Lukas Schmoigl <lukas.schmoigl@wifo.ac.at>
2025-12-16 11:18:33 +0100

    final data

---
+++ src/00_prep.R
@@ -0,0 +1,48 @@
+
+source("src/00_libs.R")
+
+data <- read_excel(
+  "data/jiec70074-sup-0003-suppmat.xlsx",
+  sheet = "Fig. 3f"
+  ) |>
+  rename(
+    value = `t CO2eq/cap`,
+    name = product_group,
+    Quantile = deciles
+  ) |>
+  mutate(
+    value = round(value * 1000),
+    Quantile = case_when(
+      Quantile == 1 ~ "Arme",
+      Quantile == 3 ~ "Untere Mittelschicht",
+      Quantile == 5 ~ "Mittelschicht",
+      Quantile == 7 ~ "Obere Mittelschicht",
+      Quantile == 10 ~ "Reiche",
+      TRUE ~ as.character(Quantile)
+    )
+  ) |>
+  mutate(
+    name = case_when(
+      name == "Food and beverages" ~ "Lebensmittel",
+      name == "Housing and energy" ~ "Wohnen",
+      name == "Housing and energy, direct" ~ "Wohnen",
+      name == "Mobility" ~ "Mobilität",
+      name == "Mobility, direct" ~ "Mobilität",
+      name == "Goods like clothing or household equipment" ~ "Güter",
+      name == "Services (accomodation, restaurants, health, recreation, etc.)" ~ "Dienstleistungen",
+      TRUE ~ name
+    )
+  ) |>
+  group_by(name, Quantile) |>
+  summarize(value = sum(value)) |>
+  filter(nchar(Quantile) > 1) |>
+  ungroup()
+
+data_agg <- data |>
+  group_by(Quantile) |>
+  summarize(value = sum(value)) |>
+  mutate(name = "Emissionen") |>
+  ungroup()
+
+add_row(data, data_agg) |>
+  write_csv("data/data.csv")
